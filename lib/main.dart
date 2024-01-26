33726108e6231660414936a9d250270157afa3a3
Bilal Ahmad <94444468+bilalahmad8739@users.noreply.github.com>
2024-01-26 16:07:44 +0500

    in main
    change in appbar

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,11 @@ void main() async{
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Payment Integration',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
