64129a18c877fddbde114b8a21e9a18a887ea0b2
Bilal Ahmad <94444468+bilalahmad8739@users.noreply.github.com>
2024-01-26 15:57:29 +0500

    added project
    added project

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:payment_integration/screens/homescreen.dart';
+
+
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Stripe.publishableKey = 'pk_test_51OZqMRSC0FUjMbaFZFCpyLmtsuakUlws7XqsOrm1ZzmQYCPiARl0x0Ir0LjPL0mfv3FD3wtd6FvQkbcdsNkFIqBK00G9IPH6C2';
+
+  await Stripe.instance.applySettings();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home:  HomeScreen(),
+    );
+  }
+}
