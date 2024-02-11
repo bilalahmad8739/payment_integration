24ad9ad9a4b693ba93166e43b9a65291bba6215f
Bilal Ahmad <94444468+bilalahmad8739@users.noreply.github.com>
2024-02-11 11:02:34 +0500

    listsearchdone

--- lib/screens/listsearch/listsearch.dart
+++ lib/screens/listsearch/listsearch.dart
@@ -11,27 +11,48 @@ class _ListSearchState extends State<ListSearch>
     with SingleTickerProviderStateMixin {
   List<Data> names = [
     Data(name: 'bilal', number: 10, rating: 4.5),
-    Data(name: 'bilal', number: 20, rating: 5.5),
-    Data(name: 'bilal', number: 8, rating: 2.5),
-    Data(name: 'bilal', number: 76, rating: 7.5),
+    Data(name: 'khan', number: 20, rating: 5.5),
+    Data(name: 'ahmad', number: 8, rating: 2.5),
+    Data(name: 'zubari', number: 76, rating: 7.5),
+    Data(name: 'abdullah', number: 20, rating: 5.5),
+    Data(name: 'adil', number: 8, rating: 2.5),
+    Data(name: 'zohaib', number: 76, rating: 7.5),
+    Data(name: 'kmran', number: 20, rating: 5.5),
+    Data(name: 'sohail', number: 8, rating: 2.5),
+    Data(name: 'sahil', number: 76, rating: 7.5),
+    Data(name: 'soduas', number: 20, rating: 5.5),
+    Data(name: 'ahmad', number: 8, rating: 2.5),
+    Data(name: 'zubari', number: 76, rating: 7.5),
   ];
-  void sortRating() {
-    names.sort(
-      (a, b) {
-        return (a.rating.compareTo(b.rating));
-      },
-    );
-    setState(() {
-      
-    });
-  }
+  List searchResult=[];
 
-  List<String> fruits = ['apple', 'mango', 'graps', 'orange'];
-  void refreshList() {
-    fruits.clear();
-    fruits.addAll(['goat', 'cow', 'lion']);
-    setState(() {});
+  void search (String query)
+  {
+   setState(() {
+     
+   searchResult = names
+          .where((item) => item.name!.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+   });
+
   }
+  // void sortRating() {
+  //   names.sort(
+  //     (a, b) {
+  //       return (a.rating.compareTo(b.rating));
+  //     },
+  //   );
+  //   setState(() {
+      
+  //   });
+  // }
+
+  // List<String> fruits = ['apple', 'mango', 'graps', 'orange'];
+  // void refreshList() {
+  //   fruits.clear();
+  //   fruits.addAll(['goat', 'cow', 'lion']);
+  //   setState(() {});
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -39,41 +60,49 @@ class _ListSearchState extends State<ListSearch>
       appBar: AppBar(
         title: Text('List of Search '),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            ListView.builder(
-                itemCount: fruits.length,
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemBuilder: ((context, index) {
-                  return Text(fruits[index]);
-                })),
-            Text(fruits.contains('apple') ? 'data' : ''),
-            SizedBox(
-              height: 50,
-            ),
-            ListView.builder(
-              itemCount: names.length,
-                scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemBuilder: ((context, index) {
-
-              return ListTile(
-                leading: Text(names[index].name.toString()),
-                subtitle: Text(names[index].number.toString()),
-                trailing: Text(names[index].rating.toStringAsFixed(2)),
-              );
-              
-            })),
-            ElevatedButton(
-                onPressed: () {
-                  // refreshList();
-                  sortRating();
-                },
-                child: Text('add'))
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              TextField(
+                onChanged: search,
+                decoration: InputDecoration(
+                  hintText: 'Search ... '
+                ),
+              ),
+              ListView.builder(
+                  itemCount: names.length,
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemBuilder: ((context, index) {
+                    return Text(names[index].name.toString(),style: TextStyle(fontSize: 50),);
+                  })),
+              // Text(names.contains('apple') ? 'data' : ''),
+              const SizedBox(
+                height: 50,
+              ),
+              // ListView.builder(
+              //   itemCount: names.length,
+              //     scrollDirection: Axis.vertical,
+              //   shrinkWrap: true,
+              //   itemBuilder: ((context, index) {
+        
+              //   return ListTile(
+              //     leading: Text(names[index].name.toString()),
+              //     subtitle: Text(names[index].number.toString()),
+              //     trailing: Text(names[index].rating.toStringAsFixed(2)),
+              //   );
+                
+              // })),
+              // ElevatedButton(
+              //     onPressed: () {
+              //       // refreshList();
+              //       // sortRating();
+              //     },
+              //     child: Text('add'))
+            ],
+          ),
         ),
       ),
     );
